0de1d30ff5eee08c643d69771b8f57995a0b5432
akapuya <akapuya@gmail.com>
2020-05-26 12:16:26 -0700

    V1

---
+++ index.js
@@ -0,0 +1,4 @@
+const JsonBufferStreamer = require('./src/JsonBufferStreamer');
+const S3Uploader = require('./src/S3Uploader');
+exports.JsonBufferStreamer = JsonBufferStreamer;
+exports.S3Uploader = S3Uploader;
